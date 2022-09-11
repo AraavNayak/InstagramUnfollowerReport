f69c61c922e8447d8e91131b72506463b89a95a0
Araav Nayak <104335810+AraavNayak@users.noreply.github.com>
2022-09-10 18:35:59 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class Main {
     
-	private static Scanner s;
+    private static Scanner s;
+	
     public static void main(String[] args) {
         s = new Scanner(System.in);
         String a = s.nextLine();
