f73f895b538ba21d7cb323e8d22e8e137e3fd86c
Araav Nayak <104335810+AraavNayak@users.noreply.github.com>
2023-01-13 09:23:07 -0800

    Update ListComparer.java

--- ListComparer.java
+++ ListComparer.java
@@ -2,7 +2,6 @@
  * Araav Nayak
  * Jan 11, 2023
  */
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -19,21 +18,18 @@ public class ListComparer {
 		try {
 			Scanner reader = new Scanner(old);
 			while(reader.hasNextLine()) {
-				
 				String s = reader.nextLine();
 				if(s.indexOf("\"") != -1) {
-					if (s.indexOf(",") == -1) {
-						oldFollowers.add(s.trim());
-					} else oldFollowers.add(s.substring(0, s.indexOf(",")).trim());
+					if (s.indexOf(",") == -1) oldFollowers.add(s.trim());
+					else oldFollowers.add(s.substring(0, s.indexOf(",")).trim());
 				}
 			}
 			reader = new Scanner(current);
 			while(reader.hasNextLine()) {
 				String s = reader.nextLine();
 				if(s.indexOf("\"") != -1) {
-					if(s.indexOf(",") == -1) {
-						currentFollowers.add(s.trim());
-					} else currentFollowers.add(s.substring(0, s.indexOf(",")).trim());
+					if(s.indexOf(",") == -1) currentFollowers.add(s.trim());
+					else currentFollowers.add(s.substring(0, s.indexOf(",")).trim());
 				}
 			}
 			reader.close();
@@ -42,22 +38,19 @@ public class ListComparer {
 			System.out.println("Usernames of ppl that have followed you since \'" + new Scanner(new File(oldSrc)).nextLine() + "\'\n" + notInOther(currentFollowers, oldFollowers));
 		} catch(Exception e) {
 			System.out.println(e.getClass() + ": " + e.getMessage());
+		} finally {
+			System.out.println("--> Done!");
 		}
 	}
 
 	public static ArrayList<Object> notInOther(ArrayList<Object> one, ArrayList<Object> two) {
 		ArrayList<Object> answer = new ArrayList<Object>();
-		
 		for(int outer = 0; outer < one.size(); outer++) {
 			boolean found = false;
 			for(int inner = 0; inner < two.size(); inner++) {
-				if(two.get(inner).equals(one.get(outer))) {
-					found = true;
-				}
+				if(two.get(inner).equals(one.get(outer))) found = true;
 			}
-			if(!found) {
-				answer.add(one.get(outer));
-			}
+			if(!found) answer.add(one.get(outer));
 		}
 		return answer;
 	}		
